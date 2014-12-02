0f378290e7d99110669cb7b2aee8a576b1e5e4be
Daniel Pyrathon <pirosb3@gmail.com>
2014-12-02 16:46:34 +0100

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 extern crate "nalgebra" as na;
 
 use std::io::BufferedReader;
+use std::num::Float;
+use std::num::pow;
+use std::num;
 use std::collections::HashMap;
 use std::io::File;
 use std::rand::distributions::{Exp, IndependentSample};
-use na::DVec;
+use na::{DVec, dot};
 
 
 #[deriving(Clone)]
 struct DataEntry {
     features: Vec<f64>,
-    target: f64
+    target: f64,
+    feature_vector: DVec<f64>
 }
 
 struct LogisticRegression {
@@ -18,30 +22,18 @@ struct LogisticRegression {
     weights: DVec<f64>
 }
 
-pub fn vector_vector_mul(data_entry: &[f64], weights: &[f64]) -> f64 {
-    let mut total: f64 = weights[0];
-
-    let len_weights = weights.len() - 1;
-    let slice_of_weights = weights.slice(1, len_weights);
-
-    for i in range(0u, len_weights) {
-        total += (data_entry[i] * weights[i]);
-    }
-    total
-}
-
 pub fn sigmoid(x : f64) -> f64 {
     let minus_x = -x;
-    1.0 / (1.0 + minus_x.exp2())
+    1.0 / (1.0 + minus_x.exp())
 }
 
 impl LogisticRegression {
     pub fn from_data_entries(dataset: &Vec<DataEntry>) -> LogisticRegression {
         let first = dataset.get(0).unwrap();
         let len = first.features.len() + 1;
-        let mut weights : Vec<f64> = Vec::new();
+        let mut weights : DVec<f64> = DVec::new_zeros(len);
         for i in range(0, len) {
-            weights.push(1.0);
+            weights[i] = 1.0;
         }
 
         LogisticRegression{
@@ -50,56 +42,44 @@ impl LogisticRegression {
         }
     }
 
-    pub fn train(&mut self, epochs: int) {
-        /*
-        let mut last_error : Option<f64> = None;
-        for i in range(0i, epochs) {
-            {
-                // Calculate hypothsies
-                let all_hypothesies : Vec<f64> = self.dataset.iter().map(|ds| {
-                    let reality = vector_vector_mul(
-                        ds.feature_vector(),
-                        self.weights.as_slice()
-                    );
-                    sigmoid(reality)
-                }).collect();
+    pub fn train(&mut self, epochs: uint) {
 
-                // Calculate error
-                if epochs % 100 == 0 {
-                    let error = range(0u, all_hypothesies.len()).map(|i| {
-                        let h_i = all_hypothesies.get(i).unwrap();
-                        let y_i = self.dataset.get(i).unwrap().target;
-                        y_i * h_i.log2() + (1.0 - y_i) * (1.0 - *h_i).log2()
-                    }).fold(0.0, |a, b| { a + b});
-                    println!("Error is: {}", error / all_hypothesies.len() as f64);
-                }
+        for i in range(0u, epochs) {
+            // Calculate Hypothesys h_i = g(w * x_i)
+            let all_hypothesis : DVec<f64> = self.dataset.iter().map(|el| {
+                let result : f64 = dot(&self.weights, &el.feature_vector);
+                sigmoid(result)
+            }).collect();
 
-                // Calculate derivatives
-                let weight_gradients : Vec<f64> = range(0u, self.weights.len()).map(|weight_id| {
+            if i % 100 == 0 {
+                let all_errors : f64 = range(0, self.dataset.len()).map(|i| {
+                    let h_i = all_hypothesis[i];
+                    let y_i = self.dataset.get(i).unwrap().target;
 
-                    range(0u, all_hypothesies.len()).map(|dataset_id| {
-                        let h_id = all_hypothesies.get(dataset_id).unwrap();
-                        let y_id = self.dataset.get(dataset_id).unwrap().target;
-
-                        let x_weight_id = {
-                            if weight_id == 0 {
-                                1.0
-                            } else {
-                                *self.dataset.get(dataset_id).unwrap().feature_vector().get(weight_id-1).unwrap()
-                            }
-                        };
-
-                        x_weight_id * (*h_id - y_id)
-                    }).fold(0.0, |a, b| { a + b })
-                }).collect();
-
-                // Update weights
-                for i in range(0u, self.weights.len()) {
-                    let mut weight = self.weights.get_mut(i).unwrap();
-                    *weight = *weight - (0.0001 * *weight_gradients.get(i).unwrap());
-                }
+                    y_i * h_i.log2() + (1.0 - y_i) * (1.0 - h_i).log2()
+                }).fold(0.0, {|a, b|  a + b }) / self.dataset.len() as f64;
+                println!("{}", all_errors);
             }
-        }*/
+
+            // Calculate per-track delta error d = (h_i - y_i)
+            let delta_errors : DVec<f64> = range(0u, self.dataset.len()).map(|i| {
+                let h_i = all_hypothesis[i];
+                let y_i = self.dataset.get(i).unwrap().target;
+                h_i - y_i
+            }).collect();
+
+            // Calculate weight delta w_i = alpha * (d * x_i)
+            let weight_updates : DVec<f64> = range(0u, self.weights.len()).map(|i| {
+                let x_i : DVec<f64> = self.dataset.iter().map(|ds| {
+                    ds.feature_vector[i]
+                }).collect();
+
+                0.001 * dot(&x_i, &delta_errors)
+            }).collect();
+
+            // Update weights
+            self.weights = self.weights - weight_updates;
+        }
     }
 }
 
@@ -108,19 +88,18 @@ impl DataEntry {
         let features: Vec<f64> = splitted_data.iter().filter_map(|el| {
             from_str(*el)
         }).collect();
+
+        let mut vec : DVec<f64> = DVec::new_zeros(features.len() + 1);
+        vec[0] = 1.0;
+        for f in range(1u, features.len()) {
+            vec[f] = *features.get(f).unwrap();
+        }
+
         DataEntry{
             features: features,
-            target: target
+            target: target,
+            feature_vector: vec,
         }
-    }
-
-    pub fn feature_vector(&self) -> &DVec<f64> {
-        let vec : DVec<f64> = DVec::new_zeros(self.features.len() + 1);
-        vec[0] = 1.0;
-        for f in range(1u, self.features.len() + 1) {
-            vec[f] = self.features.get(f).unwrap();
-        }
-        return &vec;
     }
 }
 
@@ -162,5 +141,5 @@ fn main() {
     }
 
     let mut log_regr = LogisticRegression::from_data_entries(&dataset);
-    log_regr.train(10000);
+    log_regr.train(50000);
 }
